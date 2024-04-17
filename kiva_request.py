596b4469b1722ff57abdf95a26b8a40dea983f97
user <user@users-MacBook-Air-2.local>
2024-04-17 19:18:34 -0400

    added filter

--- kiva_request.py
+++ kiva_request.py
@@ -9,7 +9,27 @@ query = f_query.read()
 # gets rid of newline
 query = query.replace('\n', '')
 
-r = requests.get(base_url+ query)
+r = requests.get(base_url + query)
 f_output = open('query_output.txt', 'w')
-json_comments = json.dump(r.json(), f_output, indent=3)
+output_json = r.json()
+json.dump(output_json, f_output, indent=3)
 f_output.close()
+
+filtered_values = {}
+# filter output_json
+if 'data' in output_json:
+    if 'lend' in output_json['data']:
+        if 'loans' in output_json['data']['lend']:
+            if 'values' in output_json['data']['lend']['loans']:
+                # dummy variable just so the output can be a dictionary
+                acc = 0
+                for value in output_json['data']['lend']['loans']['values']:
+                    length = len(value['description'].split())
+                    # could change this threshold
+                    if length >= 30:
+                        filtered_values[acc] = value
+                        acc += 1
+
+f_filtered = open('filtered_output.txt', 'w')
+json.dump(filtered_values, f_filtered, indent=3)
+f_filtered.close()
